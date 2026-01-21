c8f04b2dd8fc9fcc48a9acfd60dd9573bee5a4a1
KalleKoi <kallekoi00@gmail.com>
2026-01-21 11:05:43 +0200

    Files

---
+++ my-app/app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white' // background color
+      }}
+    >
+      <Text style={{ fontSize: 28, color: 'black' }}>
+        🎉 Hello Expo! 🎉
+      </Text>
+    </View>
+  );
+}

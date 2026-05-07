f0772ccf55dca57f12a50f140451d009ec388d29
KalleKoi <kallekoi00@gmail.com>
2026-05-07 13:22:27 +0300

    Added persistance to sign in

--- FINALPROJECT/ExpenseTracker/firebase.js
+++ FINALPROJECT/ExpenseTracker/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth/";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB53abMJ_fxuBy_uxkQxR93YpYip3-QZ7k",
@@ -15,4 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
